54d16b8dc07970d0d160c2fcbb7f322d8b0ed753
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2025-03-06 13:01:38 -0800

    started qize project

---
+++ quiz-app/script.js
@@ -0,0 +1,41 @@
+const quizData = [
+    {
+        question: "how old is humnan kind",
+        a: '1bil',
+        b: '2bil',
+        c: '3bil',
+        d: '1bil',
+        correct: 'c'
+    }, {
+        question: 'what is the most used programming language?',
+        a: 'java',
+        b: "C",
+        c: 'Pyithon',
+        d: 'javascript',
+        correct: 'a'
+
+    }, {
+        question: 'who is the current prime minister of ethiopia?',
+        a: 'Donal trump',
+        b: 'Abiy Ahmed',
+        c: 'Adanech Abebe',
+        d: 'meles zenawi',
+        correct: 'b'
+    }, {
+        question : "what does html stand for?",
+        a: ' Hyper text markup language',
+        b: 'cascading style sheet',
+        c: 'json data',
+        d: 'no answer',
+        correct: 'a'
+    }, {
+        question: 'why are you tired?',
+        a: 'because i am fasting',
+        b: 'because i didn it suhur',
+        c: 'i am working hard',
+        d: 'all are correct answers',
+        correct: 'd'
+
+    }
+    
+]
