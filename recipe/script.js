c09291aec61d528636b9b79664c8865461889511
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2025-03-07 04:47:08 -0800

    fetched the picture and name from Mealdb ApI

--- recipe/script.js
+++ recipe/script.js
@@ -19,7 +19,29 @@ async function getMealBySearch(term){
     const meals = await  fetch('https://www.themealdb.com/api/json/v1/1/random.php' + term);
 }
 
-addMeal(mealData, random = false) {
+function addMeal(mealData, random = false) {
     const meal = document.createElement('div');
     meal.classList.add('meal');
+    // console.log(random);
+
+    meal.innerHTML = `
+    <div class="meal-header">
+        ${random ? `<span class="random"> Random Recipe </span>` : ''}
+        <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+    </div>
+    <div class="meal-body">
+        <h4>${mealData.strMeal}</h4>
+        <button class="fav-btn active">
+            <i class="fas fa-heart"></i> Heart 
+        </button>
+    </div>
+    `;
+
+     meal.querySelector('.fav-btn').addEventListener('click', () => {
+        alert("why are yo doing this")
+     })
+
+    mealsEl.appendChild(meal);
+
+
 }
